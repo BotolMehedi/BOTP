a7f83318d948406aa8c8a8dd17fec1573f2f2a8b
Mehedi Hasan Ariyan <64999484+BotolMehedi@users.noreply.github.com>
2021-02-18 20:25:04 +0600

    Create botp.py

---
+++ botp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os,sys,time
+print
+print
+os.system('clear')
+print('ALL PACKAGES ARE INSTALLING. PLEASE WAIT 1 OR 2 MINUTES.....')
+print
+print
+os.system('clear')
+os.system('pkg install pip && pkg install pip2')
+time.sleep(1)
+os.system('clear')
+os.system('pip install babaotp')
+time.sleep(1)
+os.system('clear')
+os.system('pip2 install babaotp')
+time.sleep(1)
+os.system('clear')
+os.system("babaotp")
+time.sleep(1)
